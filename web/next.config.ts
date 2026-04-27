aac5e88651fd62fa1ac71cd247b10a2550c433f5
arthurlins7 <alg2@cesar.school>
2026-04-27 16:50:56 -0300

    fix: remove reactCompiler from next.config

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,18 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  experimental: {
-    reactCompiler: true,
-  },
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'cdn.nba.com',
-        pathname: '/**',
       },
     ],
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
